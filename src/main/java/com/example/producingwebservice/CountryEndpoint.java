6465621a844c1b027e4e4ab3eed082269c689875
Arek <arkadiusz.galas@gmail.com>
2020-07-28 23:33:28 +0200

    Create CountryEndpoint class.

---
+++ src/main/java/com/example/producingwebservice/CountryEndpoint.java
@@ -0,0 +1,11 @@
+package com.example.producingwebservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+
