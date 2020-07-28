ee9967bb8e990c1118f1af001c90a1ae87bdfc38
Arek <arkadiusz.galas@gmail.com>
2020-07-28 23:18:45 +0200

    Modify CountryRepository class.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# countries-soap-web-service
-SOAP web service returning country data
+#countries-soap-web-service
+SOAP web service returning country's data: name, capital, currency and population.
+
+Service can be invoked for 3 countries:
+1. Poland
+2. Spain
+3. United Kingdom
--- src/main/java/com/example/producingwebservice/CountryRepository.java
+++ src/main/java/com/example/producingwebservice/CountryRepository.java
@@ -15,6 +15,33 @@ public class CountryRepository {
 
     @PostConstruct
     public void initData() {
+        Country spain = new Country();
+        spain.setName("Spain");
+        spain.setCapital("Madrid");
+        spain.setCurrency(Currency.EUR);
+        spain.setPopulation(47737941);
 
+        countries.put(spain.getName(), spain);
+
+        Country poland = new Country();
+        poland.setName("Poland");
+        poland.setCapital("Warsaw");
+        poland.setCurrency(Currency.PLN);
+        poland.setPopulation(38346279);
+
+        countries.put(poland.getName(), poland);
+
+        Country uk = new Country();
+        uk.setName("United Kingdom");
+        uk.setCapital("London");
+        uk.setCurrency(Currency.GBP);
+        uk.setPopulation(63742977);
+
+        countries.put(uk.getName(), uk);
+    }
+
+    public Country findCountry(String name) {
+        Assert.notNull(name, "The country's name must not be null");
+        return countries.get(name);
     }
 }
